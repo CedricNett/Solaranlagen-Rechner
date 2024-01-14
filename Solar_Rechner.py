c0ff0bcc83aa8a85bc933aaaa28369809cba65c1
Cedric Nettler <103457318+CedricNett@users.noreply.github.com>
2024-01-14 02:18:37 +0100

    Update Solar_Rechner.py

--- Solar_Rechner.py
+++ Solar_Rechner.py
@@ -171,9 +171,3 @@ plot_button = ttk.Button(frame, text="Ertrag über 30 Tage anzeigen", command=pl
 plot_button.grid(row=10, columnspan=2)  # Nach der vorherigen Schaltfläche hinzufügen
 
 app.mainloop()
-
-## Echtzeit-Daten: Wenn möglich, integriere eine Funktion, die Echtzeitdaten von Sonneneinstrahlung, Temperatur oder anderen relevanten Parametern aus einer API oder einem externen Dienst abruft und in die Berechnungen einbezieht.
-
-## Responsive Design: Stelle sicher, dass die Anwendung auf verschiedenen Bildschirmgrößen und Geräten gut funktioniert, indem du ein responsives Design verwendest.
-
-## Integration von Speicherlösungen: Berücksichtige auch Speicherlösungen wie Batterien in den Berechnungen und zeige auf, wie diese die Rentabilität und Effizienz der Solaranlage beeinflussen können.
